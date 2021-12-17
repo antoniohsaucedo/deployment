cdb0b7655aad8fb44c319e1badeaba954608111d
antoniohsaucedo <antoniohsaucedo.com>
2021-12-16 20:48:09 -0700

    progress.’

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app =  express()
+const path = require('path')
+const PORT = 4004
+app.use('/js', express.static(path.join(__dirname, '../client/main.js')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/index.html'))
+  })
+
+app.listen(PORT, () => console.log(`server is running ${PORT}`))
+
