3f5804b2506c9ac009128c35d2607e4a9c80b606
antoniohsaucedo <antoniohsaucedo.com>
2021-12-16 21:02:46 -0700

    progress.’

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app =  express()
 const path = require('path')
 const PORT = 4004
 app.use('/js', express.static(path.join(__dirname, '../client/main.js')))
+app.use('/css', express.static(path.join(__dirname, '../client/index.css')))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/index.html'))
